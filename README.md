bd405cf11bc5d0b92b510ef639e613ea7c2814b9
Samuel Vijaykumar M <mad.vijay@gmail.com>
2016-12-15 19:42:12 +0530

    Fix missing comma, remove script copy, promote version

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Just include `netdata` in your node's `run_list`:
 
     MIT License
 
-    Copyright (c) [year] [fullname]
+    Copyright (c) 2016 Sambox LLP
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
--- recipes/netdata_service.rb
+++ recipes/netdata_service.rb
@@ -8,28 +8,12 @@
 
 
 
+
 execute "kill netdata" do
   command 'killall netdata'
 end
 
-execute "copy service file" do
-  command 'cp netdata-lsb /etc/init.d/netdata'
-  cwd '/tmp/netdata/system'
-end
-
-file '/etc/init.d/netdata' do
-  mode '0755'
-  owner 'root'
-  group 'root'
-  notifies :enable, "service[netdata]"
-  notifies :start, "service[netdata]"
-end
-
-execute "start netdata service" do
-  command "service netdata start"
-end
-
 service 'netdata' do
   supports :status => :true, :restart => :true, :reload => :true
-  action :nothing
+  action [:enable, :start]
 end
